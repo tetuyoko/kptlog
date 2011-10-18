e4a59dfff4fc00ab02a1c280848134705ab5515f
tetsurou yokoyama <tyokoyama53@gmail.com>
2011-10-19 00:22:01 +0900

    make project validation

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,15 @@
+# coding: utf-8
+#
 class Project < ActiveRecord::Base
   has_many :iterations
   accepts_nested_attributes_for :iterations
+
+  validates :name,
+    :presence => {
+      :message => 'は必須です。' 
+    },
+    :uniqueness => {
+      :allow_blank => true,
+      :message => 'その名称は既に使われています。'
+    } 
 end
