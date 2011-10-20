61b17a680c7f90a1d67bd02e2feaf716378a12ef
tetsurou yokoyama <tyokoyama53@gmail.com>
2011-10-21 00:39:24 +0900

    made kpts form

--- app/models/iteration.rb
+++ app/models/iteration.rb
@@ -2,7 +2,9 @@
 
 class Iteration < ActiveRecord::Base
   belongs_to :project
-  has_one :kpt
+  has_many :kpts
+  accepts_nested_attributes_for :kpts, :allow_destroy => true
+
 
   validates :name,
     :uniqueness => {
